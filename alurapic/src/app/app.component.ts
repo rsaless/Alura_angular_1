64ad919a91320e01dab48c90be7684580625e516
Rafael Sales Pavarina <rafael.pavarina@catijr.com.br>
2018-10-28 20:10:53 -0300

    início da integração com API node

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  photos = [
+    {
+      url: "https://images.unsplash.com/photo-1519575706483-221027bfbb31?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9c6d163313b444a49f7ec591b920d252&w=1000&q=80",
+      description: 'Dude 1'
+    },
+    {
+      url: "https://images.unsplash.com/photo-1519669556878-63bdad8a1a49?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2d1657bf457f194ab14b488008077f52&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb",
+      description: 'Dude 2'
+    }
+  ];
 }
