77eaef41fc87c0239b38b381eea5c43034b85116
Rafael Sales Pavarina <rafael.pavarina@catijr.com.br>
2018-10-30 14:23:54 -0300

    alterações no modulo

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  photos = [
-    {
-      url: "https://images.unsplash.com/photo-1519575706483-221027bfbb31?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9c6d163313b444a49f7ec591b920d252&w=1000&q=80",
-      description: 'Dude 1'
-    },
-    {
-      url: "https://images.unsplash.com/photo-1519669556878-63bdad8a1a49?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2d1657bf457f194ab14b488008077f52&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb",
-      description: 'Dude 2'
-    }
-  ];
+  photos = [];
+  
+  constructor(http: HttpClient){
+    
+  }
+
 }
