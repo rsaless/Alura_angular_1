7c799c26dc9169cf85aee21834a889861b02495c
Rafael Sales Pavarina <rafael.pavarina@catijr.com.br>
2018-11-01 06:01:00 -0300

    avançando no projeto

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,16 +1,8 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  photos = [];
-  
-  constructor(http: HttpClient){
-    
-  }
-
-}
+export class AppComponent{}
--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
-import { HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,8 @@ import { HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     PhotosModule,
-    HttpClientModule
+    AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
