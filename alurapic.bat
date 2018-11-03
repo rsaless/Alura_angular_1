da8ddab7aca66602b53a658f45bd255147bb420e
Rafael Sales Pavarina <rafael.pavarina@catijr.com.br>
2018-11-03 14:57:53 -0300

    diretivas e finalização da primeira etapa do projeto

---
+++ alurapic.bat
@@ -0,0 +1,5 @@
+cd Desktop
+cd github
+cd Alura_angular_1
+cd alurapic
+ng serve --open
--- alurapic/src/app/photos/photo-list/photo-list.module.ts
+++ alurapic/src/app/photos/photo-list/photo-list.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { PhotoListComponent } from './photo-list.component';
-import { PhotosComponent } from './photos/photos.component';
-import { LoadButtonComponent } from './load-button/load-button.component';
-import { FilterByDescription } from './filter-by-description.pipe';
 import { CommonModule } from '@angular/common';
+
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from 'src/app/shared/components/card/card.module';
+import { PhotosComponent } from './photos/photos.component';
+import { SearchComponent } from './search/search.component';
+import { PhotoListComponent } from './photo-list.component';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { FilterByDescription } from './filter-by-description.pipe';
+import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken-on-hover.module';
 
 @NgModule({
     declarations:[
@@ -13,11 +16,13 @@ import { CardModule } from 'src/app/shared/components/card/card.module';
         PhotosComponent,
         LoadButtonComponent,
         FilterByDescription,
+        SearchComponent
     ],
     imports:[
         CommonModule,
         PhotoModule,
         CardModule,
+        DarkenOnHoverModule,
     ]
 })
 export class PhotoListModule{
